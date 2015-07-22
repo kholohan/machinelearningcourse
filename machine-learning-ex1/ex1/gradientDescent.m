24940ff2d42d133cf59845aeda29fe4e3071d354
kholohan <kholohan@gmail.com>
2015-07-21 19:16:40 -0700

    Updated gradientDescent.m and computeCost.m

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -16,12 +16,8 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+	
+    theta = theta - (alpha/m) * (X' * (X * theta - y)); % X' is 2 x 97 and result of hypothesis - y is 97 x 1
 
     % ============================================================
 
